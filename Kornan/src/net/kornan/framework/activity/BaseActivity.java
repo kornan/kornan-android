68f6179e32aa44a23c975904986b310defc98ec2
kornan <kornan@kornan-Dell-System-Vostro-3450>
2015-08-12 16:06:48 +0800

    code

--- Kornan/src/net/kornan/framework/activity/BaseActivity.java
+++ Kornan/src/net/kornan/framework/activity/BaseActivity.java
@@ -1,15 +1,21 @@
 package net.kornan.framework.activity;
 
 import net.kornan.framework.utils.Status;
+import android.content.res.Resources;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public abstract class BaseActivity extends AppCompatActivity{
+public abstract class BaseActivity extends AppCompatActivity {
+	protected Toolbar toolbar;
 	/**
 	 * 处理网络异步数据
 	 */
-	protected Handler handler=new Handler(){
+	protected Handler handler = new Handler() {
 
 		@Override
 		public void handleMessage(Message msg) {
@@ -19,10 +25,39 @@ public abstract class BaseActivity extends AppCompatActivity{
 				setData(msg.obj);
 				break;
 			}
-			
+
 		}
-		
+
 	};
+
+	/**
+	 * toolbar
+	 */
+	public void initToolbar(String title) {
+		int id = getResources()
+				.getIdentifier("toolbar", "id", getPackageName());
+		toolbar = (Toolbar) findViewById(id);
+		toolbar.setTitle(title);// 标题的文字需在setSupportActionBar之前，不然会无效
+		setSupportActionBar(toolbar);
+		/* 这些通过ActionBar来设置也是一样的，注意要在setSupportActionBar(toolbar);之后，不然就报错了 */
+		// getSupportActionBar().setTitle("标题");
+		// getSupportActionBar().setSubtitle("副标题");
+		// getSupportActionBar().setLogo(R.drawable.ic_launcher);
+
+		/*
+		 * 菜单的监听可以在toolbar里设置，也可以像ActionBar那样，
+		 * 通过Activity的onOptionsItemSelected回调方法来处理
+		 */
+		toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+			@Override
+			public boolean onMenuItemClick(MenuItem item) {
+				Toast.makeText(getBaseContext(), "" + item.getItemId(),
+						Toast.LENGTH_LONG).show();
+				return true;
+			}
+		});
+	}
+
 	/**
 	 * 所有接收到的数据在此处理
 	 */
